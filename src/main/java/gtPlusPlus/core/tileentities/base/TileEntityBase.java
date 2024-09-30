4c297421aa1804dc2077370095c9f74cf7cf6008
miozune <miozune@gmail.com>
2024-09-30 14:36:27 +0900

    Squashing work...

--- src/main/java/gtPlusPlus/core/tileentities/base/TileEntityBase.java
+++ src/main/java/gtPlusPlus/core/tileentities/base/TileEntityBase.java
@@ -15,6 +15,8 @@ import net.minecraft.world.biome.BiomeGenBase;
 import net.minecraftforge.common.util.ForgeDirection;
 import net.minecraftforge.fluids.IFluidHandler;
 
+import org.jetbrains.annotations.NotNull;
+
 import gregtech.GTMod;
 import gregtech.api.GregTechAPI;
 import gregtech.api.enums.GTValues;
@@ -925,6 +927,7 @@ public class TileEntityBase extends TileEntity implements ILazyCoverable, IGregT
         mRedstone = aOnOff;
     }
 
+    @NotNull
     @Override
     public CoverBehavior getCoverBehaviorAtSide(ForgeDirection side) {
         return side != ForgeDirection.UNKNOWN ? mCoverBehaviors[side.ordinal()] : GregTechAPI.sNoBehavior;
